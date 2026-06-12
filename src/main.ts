cc198f0f5825c2fea99018247697192cc2098f43
Alberto La Rocca <71104@users.noreply.github.com>
2026-06-12 14:31:03 +0200

    Expose `getTextCoordinatesFromOffset`.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 // Copyright 2026 The Libernet Team
 // SPDX-License-Identifier: Apache-2.0
 
-import { parse as parseNative } from "../bindings/starkom.js";
+import { parse as parse_native, get_text_coordinates_from_offset } from "../bindings/starkom.js";
+
 import { starkom } from "./proto/ast.js";
 import ast = starkom.ast.v1;
 
+export interface TextCoordinates {
+  row: number;
+  col: number;
+}
+
+export function getTextCoordinatesFromOffset(
+  offset: number,
+  lineStarts: number[],
+): TextCoordinates {
+  let { row, col } = get_text_coordinates_from_offset(offset, Uint32Array.from(lineStarts));
+  return { row, col };
+}
+
 export function parse(path: string, input: string, withRanges: boolean): starkom.ast.v1.File {
-  const buffer = parseNative(path, input, withRanges);
+  const buffer = parse_native(path, input, withRanges);
   return ast.File.decode(buffer);
 }
