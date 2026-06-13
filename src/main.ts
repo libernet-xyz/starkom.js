bc64a40b3045389618d41c7023ad4d37ab151140
Alberto La Rocca <71104@users.noreply.github.com>
2026-06-13 03:35:02 +0200

    Add lexical tokens to the AST.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 // Copyright 2026 The Libernet Team
 // SPDX-License-Identifier: Apache-2.0
 
-import { parse as parse_native, get_text_coordinates_from_offset } from "../bindings/starkom.js";
+import {
+  parse as parse_native,
+  get_text_coordinates_from_offset,
+  ParserSettings as NativeSettings,
+} from "../bindings/starkom.js";
 
 import { starkom } from "./proto/ast.js";
 import ast = starkom.ast.v1;
@@ -9,6 +13,11 @@ import ast = starkom.ast.v1;
 export interface TextCoordinates {
   row: number;
   col: number;
+}
+
+export interface ParserSettings {
+  withTokens: boolean;
+  withRanges: boolean;
 }
 
 export function getTextCoordinatesFromOffset(
@@ -19,7 +28,11 @@ export function getTextCoordinatesFromOffset(
   return { row, col };
 }
 
-export function parse(path: string, input: string, withRanges: boolean): starkom.ast.v1.File {
-  const buffer = parse_native(path, input, withRanges);
+export function parse(
+  path: string,
+  input: string,
+  { withTokens, withRanges }: ParserSettings,
+): starkom.ast.v1.File {
+  const buffer = parse_native(path, input, new NativeSettings(withTokens, withRanges));
   return ast.File.decode(buffer);
 }
